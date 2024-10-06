dbef48f35a43678e52dbe18e223d7a93dbcbebda
Abhijeet Kumar <abhijeet145.akm@gmail.com>
2024-10-06 21:39:26 +0530

    adding 1559C

---
+++ 1300/1555C.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll n;
+    cin >> n;
+    vector<vector<int>> a(2, vector<int>(n));
+    FOR(i, 0,2) 
+        FOR(j, 0,n)
+            cin >> a[i][j];
+    ll ans = 1e9;
+    ll sum1 = 0, sum2 = 0;
+    f0tn
+        sum1 += a[0][i];
+    f0tn{
+        sum1 -= a[0][i];
+        ans = min(ans, max(sum1, sum2));
+        sum2 += a[1][i];
+    }
+    cout <<ans <<"\n";
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
+
+
+
