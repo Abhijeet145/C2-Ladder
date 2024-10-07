44b3442db3cad6cc6d070f47866313e7b6817014
Abhijeet Kumar <abhijeet145.akm@gmail.com>
2024-10-07 22:33:35 +0530

    Adds 1497C1 and C2

---
+++ 1200/1497C1.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define B begin()
+#define E end()
+#define GI greater<int>
+#define PB push_back
+#define MP make_pair
+#define LB lower_bound
+#define UB upper_bound
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(ll i=0;i<n;i++)
+
+
+////////////////////////Template ends////////////////////////
+
+
+
+
+void solve(ll tc)
+{
+    //start coding here
+    ll n,k;
+    cin >> n >> k;
+    if(n%2){
+        cout << 1 <<" "<<n/2<<" "<<n/2;
+    }else{
+        if(n%4)//not a multiple of 4
+            cout << 2<<" "<<(n-2)/2<<" "<<(n-2)/2;
+        else//multiple of 4
+            cout << n/2<<" "<<n/4<<" "<<n/4;
+
+    }
+    
+    cout <<"\n";
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t=1,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}
